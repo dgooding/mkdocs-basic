1c37a5df2143fdec72127407ce0fefbc691197c4
dgooding <dgooding@users.noreply.github.com>
2026-08-26 20:37:42 -0600

    docs: auto-update 2026-08-26 20:37:41

--- docs/javascripts/change-log-link.js
+++ docs/javascripts/change-log-link.js
@@ -1,7 +1,8 @@
 document$.subscribe(function () {
   const themeToggle = document.querySelector('[data-md-component="palette"]')
   const repositoryLink = document.querySelector('.md-header a[href="https://github.com/dgooding/mkdocs-basic"]')
-  if (!themeToggle || !repositoryLink) return
+  const repositorySource = repositoryLink && repositoryLink.closest('.md-header__source')
+  if (!themeToggle || !repositorySource) return
 
   let link = document.querySelector("[data-itsd-change-log-link]")
   if (!link) {
@@ -19,7 +20,7 @@ document$.subscribe(function () {
     tools = document.createElement("div")
     tools.className = "itsd-header-tools"
     tools.dataset.itsdHeaderTools = "true"
-    repositoryLink.insertAdjacentElement("afterend", tools)
+    repositorySource.insertAdjacentElement("afterend", tools)
   }
   tools.append(themeToggle, link)
 })
